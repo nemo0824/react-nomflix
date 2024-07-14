3e6b233e21884043628800b1dbc3b18b2b53bcbd
임재원 <nemo0824@naver.com>
2024-07-14 23:30:13 +0900

    Header

---
+++ nomflix-clone/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, BrowserRouter, Route } from 'react-router-dom';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
+import Home from './Routes/Home';
+import Header from './Components/Header';
+
+
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+      
+        <Route path="/tv" element={<Tv></Tv>}></Route>
+        <Route path="/search" element={<Search></Search>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ nomflix-clone/src/Routes/Home.tsx
@@ -0,0 +1,9 @@
+import { color } from "framer-motion";
+
+function Home(){
+    return (
+       <div style={{backgroundColor:"white", height: "200vh"}}></div>
+    )
+}
+
+export default Home;
